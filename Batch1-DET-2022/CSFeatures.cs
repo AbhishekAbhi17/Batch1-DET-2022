245639fe505beb7837b3cafddff6d0303f891187
abhishek <trabhishek17@gmail.com>
2022-08-30 17:57:56 +0530

    commited changes

---
+++ Batch1-DET-2022/CSFeatures.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class CSFeatures
+    {
+        public static void Main()
+        {
+            Action<string> action = (string name) => { Console.WriteLine($"Hai {name}"); };
+            Action<string, string> action1 = (string s1, string s2) => { Console.WriteLine($"Hai {s1}," + $"you said {s2}"); };
+            action.Invoke("abcd");
+            action1.Invoke("abcd", "am in blr");
+        }
+    
+    }
+
+}
---
+++ ClassLibrary1/ICar.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibrary1
+{
+    public class ICar
+    {
+        internal interface Icar
+        {
+            int GetPrice(string name);
+        }
+        class BMW : Icar
+        {
+            public int GetPrice(string name)
+            {
+                if (name == "M3")
+                    return 1300000;
+                else if (name == "X7")
+                    return 9600000;
+                else
+                    return 100000;
+            }
+        }
+
+    }
+}
